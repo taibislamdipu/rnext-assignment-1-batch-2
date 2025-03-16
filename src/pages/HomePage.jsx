1504f1969b2470ee376734b36507c8563709c9d9
taibislamdipu <mailtaibislam@gmail.com>
2025-03-17 03:03:50 +0600

    on progress

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Features from "../components/Features";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 
@@ -7,6 +8,7 @@ export default function HomePage() {
     <>
       <Header />
       <Hero />
+      <Features />
     </>
   );
 }
